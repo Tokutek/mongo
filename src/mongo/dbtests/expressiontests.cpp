5f861c2488a983ca4857a27335ddb451fab71f15
Pascal Borreli <pascal@borreli.com>
2012-10-14 03:57:18 +0000

    Fixed typos
    Signed-off-by: Tad Marshall <tad@10gen.com>
    
    Conflicts:
    
    	src/mongo/db/btree.h
    	src/mongo/db/diskloc.h
    	src/mongo/db/dur_preplogbuffer.cpp
    	src/mongo/db/dur_recover.cpp
    	src/mongo/db/json.cpp
    	src/mongo/db/namespace_details.h
    	src/mongo/db/record.cpp
    	src/mongo/db/repl.cpp
    	src/mongo/db/repl/rs_rollback.cpp
    	src/mongo/db/repl/rs_sync.h
    	src/mongo/dbtests/jsobjtests.cpp
    	src/mongo/dbtests/perf/btreeperf.cpp
    	src/mongo/dbtests/queryoptimizercursortests.cpp
    	src/mongo/s/d_migrate.cpp
    	src/mongo/s/d_writeback.h
    	src/mongo/util/file_allocator.h

--- src/mongo/dbtests/expressiontests.cpp
+++ src/mongo/dbtests/expressiontests.cpp
@@ -466,7 +466,7 @@ namespace ExpressionTests {
             }
         };
 
-        /** Depdencies forwarded from nested expression. */
+        /** Dependencies forwarded from nested expression. */
         class Dependencies {
         public:
             void run() {
