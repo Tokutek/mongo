df571957277992dcb01ff6ada9912aa11321bef1
Randolph Tan <randolph@10gen.com>
2012-11-05 12:42:23 -0500

    SERVER-7556 Break FailPoint library into smaller pieces
    Conflicts:
    	src/mongo/SConscript
    	src/mongo/db/db.cpp
    	src/mongo/s/server.cpp

--- src/mongo/s/server.cpp
+++ src/mongo/s/server.cpp
@@ -22,6 +22,7 @@
 #include <boost/thread/thread.hpp>
 
 #include "mongo/base/initializer.h"
+#include "mongo/db/commands/fail_point_cmd.h"
 #include "../util/net/message.h"
 #include "../util/startup_test.h"
 #include "../client/connpool.h"
@@ -45,7 +46,6 @@
 #include "cursors.h"
 #include "shard_version.h"
 #include "../util/processinfo.h"
-#include "mongo/db/fail_point_service.h"
 #include "mongo/db/lasterror.h"
 #include "mongo/plugins/loader.h"
 #include "mongo/util/stacktrace.h"
