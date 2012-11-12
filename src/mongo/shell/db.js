314bd726a7937196abb639d8dca8c4ab7f415866
Mathias Stearn <mathias@10gen.com>
2012-11-12 14:08:58 -0500

    SERVER-7168 Don't cache ensureIndex in shell

--- src/mongo/shell/db.js
+++ src/mongo/shell/db.js
@@ -361,7 +361,6 @@ DB.prototype.shutdownServer = function(opts) {
 */
 DB.prototype.cloneDatabase = function(from) { 
     assert( isString(from) && from.length );
-    //this.resetIndexCache();
     return this._dbCommand( { clone: from } );
 }
 
@@ -388,7 +387,6 @@ DB.prototype.cloneCollection = function(from, collection, query) {
     assert( isString(collection) && collection.length );
     collection = this._name + "." + collection;
     query = query || {};
-    //this.resetIndexCache();
     return this._dbCommand( { cloneCollection:collection, from:from, query:query } );
 }
 
