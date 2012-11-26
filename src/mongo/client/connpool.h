e1a5497fe1b815b841a734c734bbfeef7b17df0e
Greg Studer <greg@10gen.com>
2012-11-26 14:11:46 -0500

    SERVER-939 make scopedDbConnection take ConnectionStrings directly

--- src/mongo/client/connpool.h
+++ src/mongo/client/connpool.h
@@ -249,6 +249,10 @@ namespace mongo {
             _setSocketTimeout();
         }
 
+        explicit ScopedDbConnection(const ConnectionString& host, double socketTimeout = 0) : _host(host.toString()), _conn( pool.get(host, socketTimeout) ), _socketTimeout( socketTimeout ) {
+            _setSocketTimeout();
+        }
+
         ScopedDbConnection() : _host( "" ) , _conn(0), _socketTimeout( 0 ) {}
 
         /* @param conn - bind to an existing connection */
@@ -264,10 +268,14 @@ namespace mongo {
         // of whether or not the user is authorized, then use getInternalScopedDbConnection().
         static ScopedDbConnection* getScopedDbConnection(const string& host,
                                                          double socketTimeout = 0);
+        static ScopedDbConnection* getScopedDbConnection(const ConnectionString& host,
+                                                         double socketTimeout = 0);
         static ScopedDbConnection* getScopedDbConnection();
 
         // DEPRECATED. This is now just a synonym for getScopedDbConnection.
         static ScopedDbConnection* getInternalScopedDbConnection(const string& host,
+                                                                 double socketTimeout = 0);
+        static ScopedDbConnection* getInternalScopedDbConnection(const ConnectionString& host,
                                                                  double socketTimeout = 0);
         static ScopedDbConnection* getInternalScopedDbConnection();
 
