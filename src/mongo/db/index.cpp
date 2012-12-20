cd317a91e0465e3b0091db41cd14446b194d38a7
Kevin Matulef <matulef@10gen.com>
2012-12-20 16:25:57 -0500

    refs #544 SERVER-5848 further simplifications to default suitability() method
    Conflicts:
    
    	src/mongo/db/indexkey.cpp

--- src/mongo/db/index.cpp
+++ src/mongo/db/index.cpp
@@ -266,29 +266,26 @@ namespace mongo {
         _descriptor->generateKeys(obj, keys);
     }
 
-    static bool anyElementNamesMatch( const BSONObj& a , const BSONObj& b ) {
-        BSONObjIterator x(a);
-        while ( x.more() ) {
-            BSONElement e = x.next();
-            BSONObjIterator y(b);
-            while ( y.more() ) {
-                BSONElement f = y.next();
-                FieldCompareResult res = compareDottedFieldNames( e.fieldName() , f.fieldName() ,
-                                                                 LexNumCmp( true ) );
-                if ( res == SAME || res == LEFT_SUBFIELD || res == RIGHT_SUBFIELD )
-                    return true;
-            }
-        }
-        return false;
-    }
-
     IndexDetails::Suitability IndexDetails::suitability(const FieldRangeSet &queryConstraints,
                                                         const BSONObj &order) const {
-        if ( ! anyElementNamesMatch( _keyPattern , queryConstraints.simplifiedQuery(_keyPattern) ) &&
-             ! anyElementNamesMatch( _keyPattern , order ) )  {
-            return USELESS;
+        // This is a quick first pass to determine the suitability of the index.  It produces some
+        // false positives (returns HELPFUL for some indexes which are not particularly). When we
+        // return HELPFUL a more precise determination of utility is done by the query optimizer.
+
+        // check whether any field in the index is constrained at all by the query
+        BSONForEach( elt, _keyPattern ){
+            const FieldRange& frange = queryConstraints.range( elt.fieldName() );
+            if( ! frange.universal() )
+                return IndexDetails::HELPFUL;
         }
-        return HELPFUL;
+        // or whether any field in the desired sort order is in the index
+        set<string> orderFields;
+        order.getFieldNames( orderFields );
+        BSONForEach( k, _keyPattern ) {
+            if ( orderFields.find( k.fieldName() ) != orderFields.end() )
+                return IndexDetails::HELPFUL;
+        }
+        return IndexDetails::USELESS;
     }
 
     int IndexDetails::uniqueCheckCallback(const DBT *key, const DBT *val, void *extra) {
