1958ced0fb3f48f2c8eb304f300b6d28485e5814
Hari Khalsa <hkhalsa@10gen.com>
2013-01-16 11:14:40 -0500

    validate s2polygon loops and s2polygon proper while building it
    Conflicts:
    	jstests/geo_s2holesameasshell.js
    	jstests/geo_s2oddshapes.js
    	jstests/geo_s2polywithholes.js

--- jstests/geo_s2holesameasshell.js
+++ jstests/geo_s2holesameasshell.js
@@ -24,5 +24,22 @@ t.insert({geo: polygonWithFullHole})
 assert(db.getLastError())
 
 // No covering to search over should give an empty result set.
-res = t.find({geo: {$within: {$geometry: polygonWithFullHole}}});
-assert.eq(res.count(), 0)
+assert.throws(function() {
+    return t.find({geo: {$within: {$geometry: polygonWithFullHole}}}).count()})
+
+// Similar polygon to the one above, but is covered by two holes instead of
+// one.
+var polygonWithTwoHolesCoveringWholeArea = {"type" : "Polygon", "coordinates": [
+        [[0,0], [0,1], [1, 1], [1, 0], [0, 0]],
+        [[0,0], [0,0.5], [1, 0.5], [1, 0], [0, 0]],
+        [[0,0.5], [0,1], [1, 1], [1, 0.5], [0, 0.5]]
+    ]
+};
+
+// No keys for insert should error.
+t.insert({geo: polygonWithTwoHolesCoveringWholeArea});
+assert(db.getLastError());
+
+// No covering to search over should give an empty result set.
+assert.throws(function() {
+    return t.find({geo: {$within: {$geometry: polygonWithTwoHolesCoveringWholeArea}}}).count()})
