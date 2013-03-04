c14bbb92c5f83bc644fa6b8cf711389978312d77
John Esmet <esmet@tokutek.com>
2013-03-04 01:34:43 +0000

    refs #6124 expand test cause to use multikey index

--- jstests/remove_multicursor6124.js
+++ jstests/remove_multicursor6124.js
@@ -1,6 +1,7 @@
 t = db.remove_multicursor6124;
 t.drop()
 
+// Simple case, no multikey indexes.
 t.ensureIndex({a:1});
 t.ensureIndex({b:1});
 t.insert({_id:1, a:1, b:1})
@@ -9,3 +10,11 @@ t.insert({_id:3, a:1, b:0})
 t.remove({a:1, b:1}, false)
 
 t.drop()
+
+// Multikey case
+t.ensureIndex({a:1})
+t.ensureIndex({b:1})
+t.insert({_id:1, a:1, b:[1,2,3]})
+t.insert({_id:2, a:1, b:2})
+t.insert({_id:3, a:1, b:[2,3]})
+t.remove({a:1, b:1}, false);
