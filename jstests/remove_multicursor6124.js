7c184876e050fbcc0128ae8fe1badca3013e6f4a
John Esmet <esmet@tokutek.com>
2013-03-04 01:10:51 +0000

    refs #6124 add reproducer for 6124. if a multiplan cursor is used for delete, index cursors can assert because they can't find the associated _id document just deleted

---
+++ jstests/remove_multicursor6124.js
@@ -0,0 +1,11 @@
+t = db.remove_multicursor6124;
+t.drop()
+
+t.ensureIndex({a:1});
+t.ensureIndex({b:1});
+t.insert({_id:1, a:1, b:1})
+t.insert({_id:2, a:1, b:1})
+t.insert({_id:3, a:1, b:0})
+t.remove({a:1, b:1}, false)
+
+t.drop()
