4182004f40a6c5b78ad042ce75af729713883099
John Esmet <esmet@tokutek.com>
2013-03-04 01:58:25 +0000

    refs #5774 fix test to match our not-doing-updates-in-place behavior (though the doucment is fundamentally e qual)

--- jstests/update_arraymatch3.js
+++ jstests/update_arraymatch3.js
@@ -14,4 +14,7 @@ assert.eq( o , t.findOne() , "A1" );
 
 t.update( {'comments.by':'joe'}, {$inc:{'comments.$.votes':1}}, false, true )
 o.comments[0].votes++;
-assert.eq( o , t.findOne() , "A2" );
+o2 = t.findOne()
+assert.eq( o._id , o2._id , "A2 _id" );
+assert.eq( o.title , o2.title , "A2 title" );
+assert.eq( o.comments , o2.comments , "A2 comments" );
