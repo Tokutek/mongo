3fdd8e5d971ff315a87ee0468cb6e7d43d1657cb
Rich Prohaska <prohaska@tokutek.com>
2013-03-07 18:12:52 +0000

    refs #6200 add test case for unique index with dup keys

---
+++ jstests/6200.js
@@ -0,0 +1,13 @@
+// verify that unique index creation with duplicates fails with error code 11000
+
+t = db.jstests_6200
+t.drop();
+
+t.insert({a:1})
+t.insert({a:1})
+t.ensureIndex({a:1},{unique:1})
+e = db.getLastErrorObj()
+printjson(e)
+assert(e.code == 11000)
+
+t.drop();
