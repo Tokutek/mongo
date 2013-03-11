b9a4d46fbf4053cfcf70c077a7c4a8e31dc75083
Leif Walsh <leif@tokutek.com>
2013-03-11 22:48:06 +0000

    refs #6126 fix inc3.js until someone on the mailing list says it's a bad fix

--- jstests/inc3.js
+++ jstests/inc3.js
@@ -1,17 +1,23 @@
-print("this test will fail because we reorder the fields in more update cases than vanilla mongo.")
-print("Leif has sent mail asking about this to the mongodb-dev list")
 t = db.inc3;
+
+function assertObjsEq(a, b) {
+    for (var i in a) {
+        assert.eq(a[i], b[i], i);
+    }
+    for (var i in b) {
+        assert.eq(b[i], a[i], i);
+    }
+}
 
 t.drop();
 t.save( { _id : 1 , z : 1 , a : 1 } );
 t.update( {} , { $inc : { z : 1 , a : 1 } } );
 t.update( {} , { $inc : { a : 1 , z : 1 } } );
-assert.eq( { _id : 1 , z : 3 , a : 3 } , t.findOne() , "A" )
-
+assertObjsEq({_id:1, z:3, a:3}, t.findOne());
 
 t.drop();
 t.save( { _id : 1 , a : 1 , z : 1 } );
 t.update( {} , { $inc : { z : 1 , a : 1 } } );
 t.update( {} , { $inc : { a : 1 , z : 1 } } );
-assert.eq( { _id : 1 , a : 3 , z : 3 } , t.findOne() , "B" )
+assertObjsEq({_id:1, z:3, a:3}, t.findOne());
 
