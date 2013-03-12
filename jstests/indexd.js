95a2ed4d46be77fcfdd26f95737744696dfe4455
John Esmet <esmet@tokutek.com>
2013-03-12 18:30:21 -0400

    refs #6242 fix a test to match tokudb behavior

--- jstests/indexd.js
+++ jstests/indexd.js
@@ -4,7 +4,9 @@ t.drop();
 
 t.save( { a : 1 } );
 t.ensureIndex( { a : 1 } );
-assert.throws( function(){ db.indexd.$_id_.drop(); } );
+// TokuDB just returns false here instead of throwing an exception
+//assert.throws( function(){ db.indexd.$_id_.drop(); } );
+assert( !db.indexd.$_id_.drop() )
 assert( t.drop() );
 
 //db.indexd.$_id_.remove({});
