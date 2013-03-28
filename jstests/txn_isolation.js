4af3c872b20eef0f06a50bbc76138c7acfaf4b43
Zardosht Kasheff <zardosht@gmail.com>
2013-03-28 10:04:07 -0400

    multi statement transactions
    some progress
    
    add file
    
    implement the txn commands
    
    attempt at triaging what commands need transactions and can run in multi statement environment
    
    attempt at getting profiling right
    
    deprecate god
    
    refs #6395 add a way to create an alternate transaction stack temporarily, and use it for profile
    
    attempt at getting inserts working
    
    add error codes
    
    get compilation working of mongod working
    
    bug fixes
    
    change name of commands to have the word transaction and not txn
    
    try to fix up parameter handling of isolation level during begin
    
    fix error handling of bogus transaction isolation being passed in
    
    simplify commitTransaction, make small change to cursors
    
    small text change
    
    add test
    
    add some cases
    
    change copyright on header
    
    remove extraneous transactions
    
    comp fix
    
    make small cahnge
    
    create a dbcommands_deprecated file to isolate deprecated commands
    
    modifications
    
    attempt at settings
    
    some small changes
    
    attempt at fixing compile
    
    attempt at fixing compile
    
    some more changes
    
    compilation fixes
    
    remove _readOnly
    
    comp fix
    
    fix bug in dbeval
    
    add tests
    
    fix bug in cursor creation
    
    add simple tests to txn_isolation

--- jstests/txn_isolation.js
+++ jstests/txn_isolation.js
@@ -35,8 +35,44 @@ sleep(1000);
 r = db.runCommand({"beginTransaction":1, "isolation":"readUncommitted"});
 count = t.count();
 assert(count == 2);
+r = db.runCommand({"commitTransaction":1});
+s();
+
+// simple serializable test
+t.drop();
+t.insert({a:"before"});
+s = startParallelShell(' \
+        t = db.jstests_txn_isolation; \
+        sleep(1000); \
+        r = db.runCommand("beginTransaction"); \
+        t.insert({a:"during"}); \
+        r = db.runCommand({"commitTransaction":1}); \
+        assert(r.ok == 1);      \
+');
+
+r = db.runCommand({"beginTransaction":1, "isolation":"serializable"});
+sleep(2000);
+count = t.count();
+assert(count == 2);
 r = t.find();
 assert(r[1].a == "during");
 r = db.runCommand({"commitTransaction":1});
 s();
 
+// simple lock wait timeout test
+t.drop();
+t.insert({a:"before"});
+s = startParallelShell(' \
+        t = db.jstests_txn_isolation; \
+        r = db.runCommand("beginTransaction"); \
+        t.insert({a:"during"}); \
+        sleep(10000); \
+        r = db.runCommand({"commitTransaction":1}); \
+        assert(r.ok == 1);      \
+');
+
+r = db.runCommand({"beginTransaction":1, "isolation":"serializable"});
+sleep(1000);
+assert.throws(function(){t.count()});
+r = db.runCommand({"commitTransaction":1});
+s();
