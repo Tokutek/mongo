4da95e523b656bcc5fb9828134b9898dd6852c50
Leif Walsh <leif.walsh@gmail.com>
2013-04-03 15:51:22 -0400

    2.2.0-tokutek-0.0.3-rc.0

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -43,7 +43,7 @@ namespace mongo {
      * If you really need to do something else you'll need to fix _versionArray()
      */
     const char versionString[] = "2.2.0";
-    const char tokutekPatchVersionString[] = "0.0.3-pre-";
+    const char tokutekPatchVersionString[] = "0.0.3-rc.0";
 
     std::string fullVersionString() {
         stringstream ss;
