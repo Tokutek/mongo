12934090aea0398358d077377d04d38c832b472a
Leif Walsh <leif.walsh@gmail.com>
2013-05-30 21:07:42 -0400

    1.0.0-beta.1

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -44,7 +44,7 @@ namespace mongo {
      * If you really need to do something else you'll need to fix _versionArray()
      */
     const char mongodbVersionString[] = "2.2.4";
-    const char tokumxVersionString[] = "1.0.0-beta.0";
+    const char tokumxVersionString[] = "1.0.0-beta.1";
 
     std::string fullVersionString() {
         stringstream ss;
