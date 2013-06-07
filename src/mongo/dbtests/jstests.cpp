a22a3a6f2e1885e0d21bc0f3927c55720728f98b
Shaun Verch <shaun.verch@10gen.com>
2013-06-07 10:57:58 -0400

    SERVER-9175 Use ASSERT_OK instead of direct Status comparison

--- src/mongo/dbtests/jstests.cpp
+++ src/mongo/dbtests/jstests.cpp
@@ -1217,7 +1217,7 @@ namespace JSTests {
             virtual BSONObj bson() const {
                 BSONObjBuilder b;
                 double d;
-                ASSERT(Status::OK() == parseNumberFromString( "0.7", &d ));
+                ASSERT_OK(parseNumberFromString( "0.7", &d ));
                 b.append( "a", d );
                 return b.obj();
             }
@@ -1230,7 +1230,7 @@ namespace JSTests {
             virtual BSONObj bson() const {
                 BSONObjBuilder b;
                 double d;
-                ASSERT(Status::OK() == parseNumberFromString( "-4.4433e-2", &d ));
+                ASSERT_OK(parseNumberFromString( "-4.4433e-2", &d ));
                 b.append( "a", d );
                 return b.obj();
             }
