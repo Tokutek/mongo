79db463ac01513c80a847479087544b5d3584d05
Tad Marshall <tad@10gen.com>
2013-06-20 11:53:23 -0400

    SERVER-7080 Link to backtrace, backtrace_symbols and backtrace_symbols_fd at runtime
    Do not make direct calls to backtrace, backtrace_symbols or backtrace_symbols_fd,
    which are present in Solaris 11 but not in Solaris 10.  Instead, see if they are
    available in a loaded library (which will be libc.so.1) at runtime and either
    call them or call an emulation.

--- SConstruct
+++ SConstruct
@@ -852,7 +852,8 @@ def doConfigure(myenv):
 
     if (conf.CheckCXXHeader( "execinfo.h" ) and
         conf.CheckDeclaration('backtrace', includes='#include <execinfo.h>') and
-        conf.CheckDeclaration('backtrace_symbols', includes='#include <execinfo.h>')):
+        conf.CheckDeclaration('backtrace_symbols', includes='#include <execinfo.h>') and
+        conf.CheckDeclaration('backtrace_symbols_fd', includes='#include <execinfo.h>')):
 
         myenv.Append( CPPDEFINES=[ "MONGO_HAVE_EXECINFO_BACKTRACE" ] )
 
--- src/mongo/util/stack_introspect.cpp
+++ src/mongo/util/stack_introspect.cpp
@@ -18,21 +18,20 @@
 
 #include "mongo/util/stack_introspect.h"
 
+#if !defined(_WIN32)
+
 #include <cstdlib>
+#include <cxxabi.h>
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
 
+#include "mongo/platform/backtrace.h"
 #include "mongo/util/concurrency/mutex.h"
 #include "mongo/util/text.h"
 
 using namespace std;
-
-#ifdef MONGO_HAVE_EXECINFO_BACKTRACE
-
-#include <execinfo.h>
-#include <cxxabi.h>
 
 namespace mongo {
     
@@ -98,7 +97,7 @@ namespace mongo {
     
     bool inConstructorChain( bool printOffending ){
         void* b[maxBackTraceFrames];
-        int size = ::backtrace( b, maxBackTraceFrames );
+        int size = backtrace( b, maxBackTraceFrames );
 
         char** strings = 0;
         
@@ -114,7 +113,7 @@ namespace mongo {
             }
 
             if ( ! strings ) 
-                strings = ::backtrace_symbols( b, size );
+                strings = backtrace_symbols( b, size );
 
             string symbol = strings[i];
 
@@ -168,5 +167,4 @@ namespace mongo {
     bool inConstructorChainSupported() { return false; }
 }
 
-#endif  // defined(MONGO_HAVE_EXECINFO_BACKTRACE)
-
+#endif  // #if !defined(_WIN32)
