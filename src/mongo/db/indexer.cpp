b91ad7d733b0ecba43336c0e844bb4f769c49f33
John Esmet <john.esmet@gmail.com>
2013-08-01 10:57:07 -0400

    closes #369 Add indexer class to handle index verification, building, and commit. Capture system.indexes inserts higher in the stack so we can take different locks at different stages. We prefer to keep locking as high in the stack as possible (ie: instance.cpp)

---
+++ src/mongo/db/indexer.cpp
@@ -0,0 +1,122 @@
+/**
+*    Copyright (C) 2013 Tokutek Inc.
+*
+*    This program is free software: you can redistribute it and/or  modify
+*    it under the terms of the GNU Affero General Public License, version 3,
+*    as published by the Free Software Foundation.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "mongo/pch.h"
+
+#include "mongo/db/d_concurrency.h"
+#include "mongo/db/index.h"
+#include "mongo/db/jsobj.h"
+#include "mongo/db/namespace_details.h"
+#include "mongo/util/assert_util.h"
+#include "mongo/util/stringutils.h"
+
+namespace mongo {
+
+    NamespaceDetails::Indexer::Indexer(NamespaceDetails *d, const BSONObj &info) :
+        _d(d), _info(info), _isSecondaryIndex(_d->_nIndexes > 0) {
+
+        Lock::assertWriteLocked(_d->_ns);
+
+        const StringData &name = _info["name"].Stringdata();
+        const BSONObj &keyPattern = _info["key"].Obj();
+
+        uassert(16449, str::stream() << "dropDups is not supported and is likely to remain "
+                       << "unsupported for some time because it deletes arbitrary data",
+                       !info["dropDups"].trueValue());
+
+        uassert(12588, "cannot add index with a hot index build in progress",
+                       !_d->_indexBuildInProgress);
+
+        uassert(12523, "no index name specified",
+                        info["name"].ok());
+
+        uassert(16753, str::stream() << "index with name " << name << " already exists",
+                       _d->findIndexByName(name) < 0);
+
+        uassert(16754, str::stream() << "index already exists with diff name " <<
+                       name << " " << keyPattern.toString(),
+                       _d->findIndexByKeyPattern(keyPattern) < 0);
+
+        uassert(12505, str::stream() << "add index fails, too many indexes for " <<
+                       name << " key:" << keyPattern.toString(),
+                       _d->nIndexes() < NIndexesMax);
+
+        // Note this ns in the rollback so if this transaction aborts, we'll
+        // close this ns, forcing the next user to reload in-memory metadata.
+        NamespaceIndexRollback &rollback = cc().txn().nsIndexRollback();
+        rollback.noteNs(_d->_ns);
+
+        shared_ptr<IndexDetails> idx(new IndexDetails(_info));
+        try {
+            // Throws if we're creating an invalid index, which needs to
+            // be checked now since the ns might be created by this txn
+            // and a bad index would mean the create should be rolled back.
+            idx->getSpec();
+        } catch (...) {
+            idx->close();
+            throw;
+        }
+        _d->_indexes.push_back(idx);
+        _d->_indexBuildInProgress = true;
+    }
+
+    NamespaceDetails::Indexer::~Indexer() {
+        if (_d->_indexBuildInProgress) {
+            shared_ptr<IndexDetails> idx = _d->_indexes.back();
+            _d->_indexes.pop_back();
+            _d->_indexBuildInProgress = false;
+            verify(_d->_nIndexes == (int) _d->_indexes.size());
+            try {
+                idx->close();
+            } catch (const DBException &e) {
+                TOKULOG(0) << "Caught DBException exception while destroying Indexer: "
+                           << e.getCode() << ", " << e.what() << endl;
+            } catch (...) {
+                TOKULOG(0) << "Caught generic exception while destroying Indexer." << endl;
+            }
+        }
+    }
+
+    void NamespaceDetails::Indexer::build() {
+        Lock::assertAtLeastReadLocked(_d->_ns);
+
+        // The primary key doesn't need to be built - there's no data.
+        if (_isSecondaryIndex) {
+            IndexDetails &idx = *_d->_indexes[_d->_nIndexes];
+            _d->buildIndex(idx);
+        } 
+    }
+
+    void NamespaceDetails::Indexer::commit() {
+        Lock::assertWriteLocked(_d->_ns);
+
+        // Bumping the index count "commits" this index to the set.
+        // Setting _indexBuildInProgress to false prevents us from
+        // rolling back the index creation in the destructor.
+        _d->_indexBuildInProgress = false;
+        _d->_nIndexes++;
+
+        // The first index we create should be the pk index, when we first create the collection.
+        // Therefore the collection's NamespaceDetails should not already exist in the NamespaceIndex
+        // unless we are building a secondary index (and therefore the collection already exists)
+        if (!_isSecondaryIndex) {
+            massert(16435, "first index should be pk index", _info["key"].Obj() == _d->_pk);
+        }
+        nsindex(_d->_ns)->update_ns(_d->_ns, _d->serialize(), _isSecondaryIndex);
+        NamespaceDetailsTransient::get(_d->_ns).addedIndex();
+    }
+
+} // namespace mongo
--- src/mongo/db/instance.cpp
+++ src/mongo/db/instance.cpp
@@ -756,6 +756,78 @@ namespace mongo {
         return ok;
     }
 
+    static void _receivedSystemIndexesInsert(const char *ns, Message &m, const vector<BSONObj> objs) {
+        uassert(16905, "Can only build one index at a time.", objs.size() == 1);
+
+        Client::Transaction transaction(DB_SERIALIZABLE);
+        scoped_ptr<NamespaceDetails::Indexer> indexer;
+
+        // This is the magic write lock that prevents the hot indexing
+        // scaffolding from breaking the existing "cold" indexing logic.
+        Lock::DBWrite lk(ns);
+
+        try {
+            Lock::DBWrite lk(ns);
+            uassert(16902, "not master", isMasterNs(ns));
+
+            if (handlePossibleShardedMessage(m, 0)) {
+                return;
+            }
+
+            const BSONObj &info = objs[0];
+            const StringData &coll = info["ns"].Stringdata();
+
+            Client::Context ctx(ns);
+            NamespaceDetails *d = getAndMaybeCreateNS(coll, true);
+            uassert(16903, str::stream() << "Cannot build an index on a namespace under-going bulk load: " << ns,
+                           cc().bulkLoadNS() != coll);
+
+            if (d->findIndexByKeyPattern(info["key"].Obj()) >= 0) {
+                // No error or action if the index already exists. We need to commit
+                // the transaction in case this is an ensure index on the _id field
+                // and the ns was created by getAndMaybeCreateNS()
+                transaction.commit();
+                return;
+            }
+
+            _insertObjects(ns, objs, false, 0, true);
+            indexer.reset(new NamespaceDetails::Indexer(d, info));
+        } catch (...) {
+            // The indexer destructor must be called in a write lock
+            Lock::DBWrite lk(ns);
+            indexer.reset();
+            throw;
+        }
+
+        try {
+            Lock::DBWrite lk(ns);
+            uassert(16906, "not master: after indexer setup but before build", isMasterNs(ns));
+
+            Client::Context ctx(ns);
+            indexer->build();
+        } catch (...) {
+            // The indexer destructor must be called in a write lock
+            Lock::DBWrite lk(ns);
+            indexer.reset();
+            throw;
+        }
+
+        try {
+            Lock::DBWrite lk(ns);
+            uassert(16907, "not master: after indexer build but before commit", isMasterNs(ns));
+
+            Client::Context ctx(ns);
+            indexer->commit();
+        } catch (...) {
+            // The indexer destructor must be called in a write lock
+            Lock::DBWrite lk(ns);
+            indexer.reset();
+            throw;
+        }
+
+        transaction.commit();
+    }
+
     static void lockedReceivedInsert(const char *ns, Message &m, const vector<BSONObj> &objs, const bool keepGoing) {
         // writelock is used to synchronize stepdowns w/ writes
         uassert(10058, "not master", isMasterNs(ns));
@@ -791,6 +863,11 @@ namespace mongo {
         OpSettings settings;
         settings.setQueryCursorMode(WRITE_LOCK_CURSOR);
         cc().setOpSettings(settings);
+
+        if (str::contains(ns, ".system.indexes")) {
+            _receivedSystemIndexesInsert(ns, m, objs);
+            return;
+        }
 
         try {
             Lock::DBRead lk(ns);
