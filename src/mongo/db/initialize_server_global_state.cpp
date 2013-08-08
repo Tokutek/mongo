86b7edbf944466fbe563a7b18f7b2f8bf148206d
Shaun Verch <shaun.verch@10gen.com>
2013-08-08 15:57:56 -0400

    SERVER-8510 Remove configuration variable manager and add option group for command line parsing
    Conflicts:
    	src/mongo/db/mongod.vcxproj
    	src/mongo/db/mongod.vcxproj.filters
    	src/mongo/dbtests/test.vcxproj
    	src/mongo/dbtests/test.vcxproj.filters
    	src/mongo/s/mongos.vcxproj
    	src/mongo/s/mongos_v8.vcxproj.filters
    	src/mongo/shell/mongo.vcxproj
    	src/mongo/shell/mongo.vcxproj.filters

--- src/mongo/db/initialize_server_global_state.cpp
+++ src/mongo/db/initialize_server_global_state.cpp
@@ -156,7 +156,7 @@ namespace mongo {
     }
 
     MONGO_INITIALIZER_GENERAL(ServerLogRedirection,
-                              ("GlobalLogManager", "globalVariablesConfigured"),
+                              ("GlobalLogManager", "completedStartupConfig"),
                               ("default"))(
             InitializerContext*) {
 
