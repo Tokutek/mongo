a63136d24f87bdd466e632daec571e5612846436
Leif Walsh <leif.walsh@gmail.com>
2013-08-13 17:19:53 -0400

    tokumx-1.0.4-rc.0

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -44,7 +44,7 @@ namespace mongo {
      * If you really need to do something else you'll need to fix _versionArray()
      */
     const char mongodbVersionString[] = "2.2.5";
-    const char tokumxVersionString[] = "1.0.4-pre-";
+    const char tokumxVersionString[] = "1.0.4-rc.0";
 
     std::string fullVersionString() {
         stringstream ss;
