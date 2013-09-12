9a5178d466cfcde6be53a22d3a6a29da95958e8e
Spencer T Brody <spencer@10gen.com>
2013-09-12 15:18:39 -0400

    SERVER-9518 Simplify locking of authorization documents

--- src/mongo/db/auth/authz_manager_external_state_s.h
+++ src/mongo/db/auth/authz_manager_external_state_s.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <boost/function.hpp>
+#include <boost/thread/mutex.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <string>
 
@@ -93,6 +94,7 @@ namespace mongo {
                                  BSONObj* result);
 
     private:
+        boost::mutex _distLockGuard; // Guards access to _authzDataUpdateLock
         scoped_ptr<ScopedDistributedLock> _authzDataUpdateLock;
     };
 
