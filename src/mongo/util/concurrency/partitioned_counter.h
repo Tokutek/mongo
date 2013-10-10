97c52321cff433c1886a9f1072bc833b20ade9af
Leif Walsh <leif.walsh@gmail.com>
2013-10-10 00:03:02 -0400

    fix assert id #598

--- src/mongo/util/concurrency/partitioned_counter.h
+++ src/mongo/util/concurrency/partitioned_counter.h
@@ -166,7 +166,7 @@ namespace mongo {
     template<typename Value>
     PartitionedCounter<Value>& PartitionedCounter<Value>::dec(Value x) {
         if (!std::numeric_limits<Value>::is_signed) {
-            massert(17019, "cannot decrement partitioned counter below zero", ts()._sum > x);
+            massert(17020, "cannot decrement partitioned counter below zero", ts()._sum > x);
         }
         ts()._sum -= x;
         return *this;
