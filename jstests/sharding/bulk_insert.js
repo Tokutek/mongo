894d67bff42e937bed8d2de5890c1746a6c6ddf3
Leif Walsh <leif.walsh@gmail.com>
2013-10-12 22:54:38 -0400

    fixed syntax in bulk_insert.js #579

--- jstests/sharding/bulk_insert.js
+++ jstests/sharding/bulk_insert.js
@@ -38,7 +38,7 @@ printjson(collUn.ensureIndex({ukey : 1}, {unique : true, clustering : true}));
 printjson(collDi.ensureIndex({ukey : 1}, {unique : true, clustering : true}));
 
 printjson(admin.runCommand({shardCollection : collSh + "",
-                            key : {ukey : 1}})
+                            key : {ukey : 1}}))
 printjson(admin.runCommand({split : collSh + "",
                             middle : {ukey : 0}}));
 printjson(admin.runCommand({moveChunk : collSh + "",
