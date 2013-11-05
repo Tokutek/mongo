817fad58bb7a26acea7d2191c17d7a645bf2e74a
Benety Goh <benety@mongodb.com>
2013-11-04 20:04:04 -0500

    SERVER-10416 support DBRef as $in operand

--- src/mongo/db/matcher/expression_parser.h
+++ src/mongo/db/matcher/expression_parser.h
@@ -51,6 +51,8 @@ namespace mongo {
          */
         static bool _isExpressionDocument( const BSONElement& e );
 
+        static bool _isDBRefDocument( const BSONObj& obj );
+
         static StatusWithMatchExpression _parse( const BSONObj& obj, bool topLevel );
 
         /**
