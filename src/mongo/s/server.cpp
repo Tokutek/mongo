568054c934697046321dc094367dd5c5371eb6db
Leif Walsh <leif.walsh@gmail.com>
2013-11-08 13:13:34 -0500

    always handle SIGTERM and SIGHUP #697 #706
    We don't really need to keep the default SIGTERM and SIGHUP handlers when
    running in gdb.  Discovered while working on #697

--- src/mongo/s/server.cpp
+++ src/mongo/s/server.cpp
@@ -198,23 +198,23 @@ namespace mongo {
         setupSIGTRAPforGDB();
         setupCoreSignals();
 
+        signal(SIGTERM, sighandler);
         if (!cmdLine.gdb) {
-            signal(SIGTERM, sighandler);
             signal(SIGINT, sighandler);
+        }
 
 #if defined(SIGQUIT)
-            signal( SIGQUIT , printStackAndExit );
+        signal( SIGQUIT , printStackAndExit );
 #endif
-            signal( SIGSEGV , printStackAndExit );
-            signal( SIGABRT , printStackAndExit );
-            signal( SIGFPE , printStackAndExit );
+        signal( SIGSEGV , printStackAndExit );
+        signal( SIGABRT , printStackAndExit );
+        signal( SIGFPE , printStackAndExit );
 #if defined(SIGBUS)
-            signal( SIGBUS , printStackAndExit );
+        signal( SIGBUS , printStackAndExit );
 #endif
 #if defined(SIGPIPE)
-            signal( SIGPIPE , SIG_IGN );
+        signal( SIGPIPE , SIG_IGN );
 #endif
-        }
 
 #ifndef _WIN32
         sigemptyset( &asyncSignals );
