313c64fd82c7815c91e55ca6d4f0e2c7fae782e7
Zardosht Kasheff <zardosht@gmail.com>
2013-11-27 14:41:16 -0500

    refs #787, add a case to a test that exposes the problem

--- jstests/count.js
+++ jstests/count.js
@@ -23,3 +23,6 @@ t.ensureIndex( {b:1,a:1,c:1} );
 assert.eq( 1, t.find( {a:true,b:false} ).count() , "I" );
 assert.eq( 1, t.find( {b:false,a:true} ).count() , "J" );
 
+t.drop();
+t.insert([{_id:1},{_id:2},{_id:3},{_id:4},{_id:5},{_id:6},{_id:7}]);
+assert.eq(3, t.find({ _id : { $gt:2 , $lt:6} }).count());
