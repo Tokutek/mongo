8267c7b17a2ccbf77dbe2d28e5e04e7beebaae83
John Esmet <esmet@tokutek.com>
2013-12-08 18:20:17 -0500

    fixes #180 deprecate fsync+lock, but keep the interface around for legacy purposes

--- src/mongo/db/commands/fsync.cpp
+++ src/mongo/db/commands/fsync.cpp
@@ -35,39 +35,9 @@
 
 namespace mongo {
     
-    class FSyncLockThread : public BackgroundJob {
-        void doRealWork();
-    public:
-        FSyncLockThread() : BackgroundJob( true ) {}
-        virtual ~FSyncLockThread(){}
-        virtual string name() const { return "FSyncLockThread"; }
-        virtual void run() {
-            Client::initThread( "fsyncLockWorker" );
-            try {
-                doRealWork();
-            }
-            catch ( std::exception& e ) {
-                error() << "FSyncLockThread exception: " << e.what() << endl;
-            }
-            cc().shutdown();
-        }
-    };
-
-    /* see unlockFsync() for unlocking:
-       db.$cmd.sys.unlock.findOne()
-    */
     class FSyncCommand : public Command {
     public:
-        static const char* url() { return "http://dochub.mongodb.org/core/fsynccommand"; }
-        bool locked;
-        bool pendingUnlock;
-        SimpleMutex m; // protects locked var above
-        string err;
-
-        boost::condition _threadSync;
-        boost::condition _unlockSync;
-
-        FSyncCommand() : Command( "fsync" ), m("lockfsync") { locked=false; pendingUnlock=false; }
+        FSyncCommand() : Command("fsync") {}
         virtual LockType locktype() const { return NONE; }
         virtual bool slaveOk() const { return true; }
         virtual bool requiresSync() const { return false; }
@@ -76,7 +46,7 @@ namespace mongo {
         virtual bool canRunInMultiStmtTxn() const { return true; }
         virtual OpSettings getOpSettings() const { return OpSettings(); }
         virtual bool adminOnly() const { return true; }
-        virtual void help(stringstream& h) const { h << url(); }
+        virtual void help(stringstream& h) const { h << "http://dochub.mongodb.org/core/fsynccommand"; }
         virtual void addRequiredPrivileges(const std::string& dbname,
                                            const BSONObj& cmdObj,
                                            std::vector<Privilege>* out) {
@@ -85,120 +55,14 @@ namespace mongo {
             out->push_back(Privilege(AuthorizationManager::SERVER_RESOURCE_NAME, actions));
         }
         virtual bool run(const string& dbname, BSONObj& cmdObj, int, string& errmsg, BSONObjBuilder& result, bool fromRepl) {
-
-            if (Lock::isLocked()) {
-                errmsg = "fsync: Cannot execute fsync command from contexts that hold a data lock";
-                return false;
+            const bool lock = cmdObj["lock"].trueValue();
+            if (lock) {
+                const char *msg = "fsync with lock:true is not suitable for locked snapshotting in TokuMX";
+                warning() << msg << endl;
+                result.append("info", msg);
             }
-
-            bool sync = !cmdObj["async"].trueValue(); // async means do an fsync, but return immediately
-            bool lock = cmdObj["lock"].trueValue();
-            if( lock ) {
-                if ( ! sync ) {
-                    errmsg = "fsync: sync option must be true when using lock";
-                    return false;
-                }
-
-                SimpleMutex::scoped_lock lk(m);
-                err = "";
-                
-                (new FSyncLockThread())->go();
-                while ( ! locked && err.size() == 0 ) {
-                    _threadSync.wait( m );
-                }
-                
-                if ( err.size() ){
-                    errmsg = err;
-                    return false;
-                }
-                
-                log() << "db is now locked for snapshotting, no writes allowed. db.fsyncUnlock() to unlock" << endl;
-                log() << "    For more info see " << FSyncCommand::url() << endl;
-                result.append("info", "now locked against writes, use db.fsyncUnlock() to unlock");
-                result.append("seeAlso", FSyncCommand::url());
-
-            }
-            else {
-                // the simple fsync command case
-                if (sync) {
-                    Lock::GlobalWrite w; // can this be GlobalRead? and if it can, it should be nongreedy.
-                    DEV LOG(0) << "in fsync: flushAll/commitNow not implemented, doing nothing!" << endl;
-                }
-                // question : is it ok this is not in the dblock? i think so but this is a change from past behavior, 
-                // please advise.
-                DEV LOG(0) << "in fsync: number of files flushed not known, arbitrarily reporting 1!" << endl;
-                result.append( "numFiles" , 1 );
-            }
-            return 1;
+            return true;
         }
     } fsyncCmd;
 
-    SimpleMutex filesLockedFsync("filesLockedFsync");
-
-    void FSyncLockThread::doRealWork() {
-        SimpleMutex::scoped_lock lkf(filesLockedFsync);
-        Lock::GlobalWrite global(true/*stopGreed*/);
-        SimpleMutex::scoped_lock lk(fsyncCmd.m);
-        
-        verify( ! fsyncCmd.locked ); // impossible to get here if locked is true
-        try { 
-            problem() << " syncDataAndTruncateJournal not implemented, doing nothing!" << endl;
-        } 
-        catch( std::exception& e ) { 
-            error() << "error doing syncDataAndTruncateJournal: " << e.what() << endl;
-            fsyncCmd.err = e.what();
-            fsyncCmd._threadSync.notify_one();
-            fsyncCmd.locked = false;
-            return;
-        }
-        
-        global.downgrade();
-        
-        try {
-            problem() << " flushAll not implemented, doing nothing!" << endl;
-        }
-        catch( std::exception& e ) { 
-            error() << "error doing flushAll: " << e.what() << endl;
-            fsyncCmd.err = e.what();
-            fsyncCmd._threadSync.notify_one();
-            fsyncCmd.locked = false;
-            return;
-        }
-
-        verify( ! fsyncCmd.locked );
-        fsyncCmd.locked = true;
-        
-        fsyncCmd._threadSync.notify_one();
-
-        while ( ! fsyncCmd.pendingUnlock ) {
-            fsyncCmd._unlockSync.wait(fsyncCmd.m);
-        }
-        fsyncCmd.pendingUnlock = false;
-        
-        fsyncCmd.locked = false;
-        fsyncCmd.err = "unlocked";
-
-        fsyncCmd._unlockSync.notify_one();
-    }
-
-    bool lockedForWriting() { 
-        return fsyncCmd.locked; 
-    }
-    
-    // @return true if unlocked
-    bool _unlockFsync() {
-        verify(!Lock::isLocked());
-        SimpleMutex::scoped_lock lk( fsyncCmd.m );
-        if( !fsyncCmd.locked ) { 
-            return false;
-        }
-        fsyncCmd.pendingUnlock = true;
-        fsyncCmd._unlockSync.notify_one();
-        fsyncCmd._threadSync.notify_one();
-        
-        while ( fsyncCmd.locked ) {
-            fsyncCmd._unlockSync.wait( fsyncCmd.m );
-        }
-        return true;
-    }
-}
+} // namespace mongo
