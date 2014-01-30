9c1de880ee4ef8acb5279bfdacddb840320dcd5d
Leif Walsh <leif.walsh@gmail.com>
2014-01-29 23:31:42 -0500

    attempted to make cursor8 less touchy #939

--- jstests/cursor8.js
+++ jstests/cursor8.js
@@ -6,14 +6,19 @@ t.save( {} );
 t.save( {} );
 t.save( {} );
 
+function get() {
+    var res = db.runCommand({cursorInfo:1});
+    return res.clientCursors_size;
+}
+
 function test( want , msg ){
     var res = db.runCommand( { cursorInfo:1 } );
     assert.eq( want , res.clientCursors_size , msg + " " + tojson( res ) );
 }
 
-test( 0 , "A1" );
+var before = get();
 assert.eq( 3 , t.find().count() , "A2" );
 assert.eq( 3 , t.find( {} ).count() , "A3" );
 assert.eq( 2, t.find( {} ).limit( 2 ).itcount() , "A4" );
-test( 1 , "B1" );
+test( before + 1 , "B1" );
 
