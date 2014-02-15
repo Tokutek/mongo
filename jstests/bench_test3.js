0c91a2a19936679e47c98aae770be6c522e16e91
Leif Walsh <leif.walsh@gmail.com>
2014-02-15 10:45:51 -0500

    made bench_test3 less flaky, just have it retry a few times #869

--- jstests/bench_test3.js
+++ jstests/bench_test3.js
@@ -1,5 +1,4 @@
 t = db.bench_test3
-t.drop();
 
 
 benchArgs = { ops : [ { ns : t.getFullName() ,
@@ -18,10 +17,26 @@ if (jsTest.options().auth) {
     benchArgs['password'] = jsTest.options().adminPassword;
 }
 
-res = benchRun( benchArgs )
-printjson( res );
+for (var i = 0; i < 5; ++i) {
+    t.drop();
 
-var keys = []
-var totals = {}
-db.bench_test3.find().sort( { _id : 1 } ).forEach( function(z){ keys.push( z._id ); totals[z._id] = z.x } );
-assert.eq( [ 0 , 4 , 8 , 12 , 16 ] , keys )
+    res = benchRun( benchArgs )
+    printjson( res );
+
+    var keys = []
+    var totals = {}
+    db.bench_test3.find().sort( { _id : 1 } ).forEach( function(z){ keys.push( z._id ); totals[z._id] = z.x } );
+
+    // Rather than do this assert, since the updates sometimes get lock not granted, we try a few
+    // times before failing the test
+    // assert.eq( [ 0 , 4 , 8 , 12 , 16 ] , keys )
+
+    if (keys == [ 0 , 4 , 8 , 12 , 16 ]) {
+        // pass
+        break;
+    }
+
+    if (i == 4) {
+        assert.eq( [ 0 , 4 , 8 , 12 , 16 ] , keys );
+    }
+}
