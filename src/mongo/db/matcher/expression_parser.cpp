5c4cae4ab11f6054c85ae628142c534540988847
David Storch <david.storch@10gen.com>
2014-03-20 15:17:23 -0400

    SERVER-10026 limit tree depth allowed by the match expression parser
    (cherry picked from commit 926d45b2cd43cf255f0994b9da8e518bc381b794)

--- src/mongo/db/matcher/expression_parser.cpp
+++ src/mongo/db/matcher/expression_parser.cpp
@@ -242,9 +242,16 @@ namespace mongo {
                                           mongoutils::str::stream() << "not handled: " << e.fieldName() );
     }
 
-    StatusWithMatchExpression MatchExpressionParser::_parse( const BSONObj& obj, bool topLevel ) {
+    StatusWithMatchExpression MatchExpressionParser::_parse( const BSONObj& obj, int level ) {
+        if (level > kMaximumTreeDepth) {
+            return StatusWithMatchExpression( ErrorCodes::BadValue,
+                                              "exceeded maximum query tree depth" );
+        }
 
         std::auto_ptr<AndMatchExpression> root( new AndMatchExpression() );
+
+        bool topLevel = (level == 0);
+        level++;
 
         BSONObjIterator i( obj );
         while ( i.more() ){
@@ -259,7 +266,7 @@ namespace mongo {
                         return StatusWithMatchExpression( ErrorCodes::BadValue,
                                                      "$or needs an array" );
                     std::auto_ptr<OrMatchExpression> temp( new OrMatchExpression() );
-                    Status s = _parseTreeList( e.Obj(), temp.get() );
+                    Status s = _parseTreeList( e.Obj(), temp.get(), level );
                     if ( !s.isOK() )
                         return StatusWithMatchExpression( s );
                     root->add( temp.release() );
@@ -269,7 +276,7 @@ namespace mongo {
                         return StatusWithMatchExpression( ErrorCodes::BadValue,
                                                      "and needs an array" );
                     std::auto_ptr<AndMatchExpression> temp( new AndMatchExpression() );
-                    Status s = _parseTreeList( e.Obj(), temp.get() );
+                    Status s = _parseTreeList( e.Obj(), temp.get(), level );
                     if ( !s.isOK() )
                         return StatusWithMatchExpression( s );
                     root->add( temp.release() );
@@ -279,7 +286,7 @@ namespace mongo {
                         return StatusWithMatchExpression( ErrorCodes::BadValue,
                                                      "and needs an array" );
                     std::auto_ptr<NorMatchExpression> temp( new NorMatchExpression() );
-                    Status s = _parseTreeList( e.Obj(), temp.get() );
+                    Status s = _parseTreeList( e.Obj(), temp.get(), level );
                     if ( !s.isOK() )
                         return StatusWithMatchExpression( s );
                     root->add( temp.release() );
