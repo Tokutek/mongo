b8a4676f723e573adf884bc57889020ae31d5cf9
John Esmet <john.esmet@gmail.com>
2014-08-05 11:45:54 -0400

    Manual fixup of instrospect.*

--- src/mongo/db/introspect.cpp
+++ src/mongo/db/introspect.cpp
@@ -15,29 +15,48 @@
 *
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    As a special exception, the copyright holders give permission to link the
+*    code of portions of this program with the OpenSSL library under certain
+*    conditions as described in each individual source file and distribute
+*    linked combinations including the program with the OpenSSL library. You
+*    must comply with the GNU Affero General Public License in all respects for
+*    all of the code used other than as permitted herein. If you modify file(s)
+*    with this exception, you may extend this exception to your version of the
+*    file(s), but you are not obligated to do so. If you do not wish to do so,
+*    delete this exception statement from your version. If you delete this
+*    exception statement from all source files in the program, then also delete
+*    it in the license file.
 */
 
 #include "mongo/pch.h"
 
 #include "mongo/bson/util/builder.h"
 #include "mongo/db/auth/authorization_manager.h"
-#include "mongo/db/auth/principal_set.h"
+#include "mongo/db/auth/authorization_session.h"
+#include "mongo/db/auth/user_set.h"
 #include "mongo/db/curop.h"
 #include "mongo/db/database.h"
 #include "mongo/db/databaseholder.h"
 #include "mongo/db/introspect.h"
-#include "mongo/db/curop.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/ops/insert.h"
+#include "mongo/db/storage_options.h"
 #include "mongo/util/goodies.h"
+
+namespace {
+    const size_t MAX_PROFILE_DOC_SIZE_BYTES = 100*1024;
+}
 
 namespace mongo {
 
 namespace {
-    void _appendUserInfo(const Client& c, BSONObjBuilder& builder, AuthorizationManager* authManager) {
-        PrincipalSet::NameIterator nameIter = authManager->getAuthenticatedPrincipalNames();
+    void _appendUserInfo(const Client& c,
+                         BSONObjBuilder& builder,
+                         AuthorizationSession* authSession) {
+        UserNameIterator nameIter = authSession->getAuthenticatedUserNames();
 
-        PrincipalName bestUser;
+        UserName bestUser;
         if (nameIter.more())
             bestUser = *nameIter;
 
@@ -47,7 +66,7 @@ namespace {
         for ( ; nameIter.more(); nameIter.next()) {
             BSONObjBuilder nextUser(allUsers.subobjStart());
             nextUser.append(AuthorizationManager::USER_NAME_FIELD_NAME, nameIter->getUser());
-            nextUser.append(AuthorizationManager::USER_SOURCE_FIELD_NAME, nameIter->getDB());
+            nextUser.append(AuthorizationManager::USER_DB_FIELD_NAME, nameIter->getDB());
             nextUser.doneFast();
 
             if (nameIter->getDB() == opdb) {
@@ -67,37 +86,43 @@ namespace {
 
         // build object
         BSONObjBuilder b(profileBufBuilder);
+
+        const bool isQueryObjTooBig = !currentOp.debug().append(currentOp, b,
+                MAX_PROFILE_DOC_SIZE_BYTES);
+
         b.appendDate("ts", jsTime());
-        currentOp.debug().append( currentOp , b );
         b.append("client", c.clientAddress());
 
-        AuthorizationManager* authManager = c.getAuthorizationManager();
-        _appendUserInfo(c, b, authManager);
+        AuthorizationSession * authSession = c.getAuthorizationSession();
+        _appendUserInfo(c, b, authSession);
 
         BSONObj p = b.done();
 
-        if (p.objsize() > 100*1024){
+        if (static_cast<size_t>(p.objsize()) > MAX_PROFILE_DOC_SIZE_BYTES || isQueryObjTooBig) {
             string small = p.toString(/*isArray*/false, /*full*/false);
 
-            warning() << "can't add full line to system.profile: " << small;
+            warning() << "can't add full line to system.profile: " << small << endl;
 
             // rebuild with limited info
             BSONObjBuilder b(profileBufBuilder);
             b.appendDate("ts", jsTime());
             b.append("client", c.clientAddress() );
-            _appendUserInfo(c, b, authManager);
+            _appendUserInfo(c, b, authSession);
 
             b.append("err", "profile line too large (max is 100KB)");
-            if (small.size() < 100*1024){ // should be much smaller but if not don't break anything
+
+            // should be much smaller but if not don't break anything
+            if (small.size() < MAX_PROFILE_DOC_SIZE_BYTES){
                 b.append("abbreviated", small);
             }
 
             p = b.done();
         }
 
+        // write: not replicated
         // get or create the profiling collection
-        Collection *cl = getOrCreateProfileCollection(db);
-        if (cl) {
+        Collection* profileCollection = getOrCreateProfileCollection(db);
+        if ( profileCollection ) {
             insertOneObject(cl, p);
         }
     }
@@ -119,31 +144,49 @@ namespace {
         }
     }
 
-    Collection *getOrCreateProfileCollection(Database *db, bool force) {
+    Collection* getOrCreateProfileCollection(Database *db, bool force, string* errmsg ) {
         fassert(16372, db);
-        const char *profileName = db->profileName().c_str();
-        Collection *cl = getCollection(profileName);
-        if (!cl && (cmdLine.defaultProfile || force)) {
-            // system.profile collection doesn't exist; create it
-            log() << "creating profile collection: " << profileName << endl;
-            string errmsg;
-            if (!userCreateNS(profileName,
-                              BSON("capped" << true << "size" << 1024 * 1024 << "autoIndexId" << false),
-                              errmsg , false)) {
-                log() << "could not create ns " << profileName << ": " << errmsg << endl;
+        const char* profileName = db->profileName().c_str();
+        Collection* collection = getCollection(profileName);
+
+        if ( collection ) {
+            if ( !collection->isCapped() ) {
+                string myerrmsg = str::stream() << profileName << " exists but isn't capped";
+                log() << myerrmsg << endl;
+                if ( errmsg )
+                    *errmsg = myerrmsg;
                 return NULL;
             }
-            cl = getCollection(profileName);
+            return collection;
         }
-        if (!cl) {
-            // failed to get or create profile collection
+
+        // does not exist!
+
+        if ( force == false && serverGlobalParams.defaultProfile == false ) {
+            // we don't want it, so why are we here?
             static time_t last = time(0) - 10;  // warn the first time
             if( time(0) > last+10 ) {
                 log() << "profile: warning ns " << profileName << " does not exist" << endl;
                 last = time(0);
             }
+            return NULL;
         }
-        return cl;
+
+        // system.profile namespace doesn't exist; create it
+        log() << "creating profile collection: " << profileName << endl;
+        string myerrmsg;
+        if (!userCreateNS(profileName,
+                          BSON("capped" << true << "size" << 1024 * 1024), myerrmsg , false)) {
+            myerrmsg = str::stream() << "could not create ns " << profileName << ": " << myerrmsg;
+            log() << myerrmsg << endl;
+            if ( errmsg )
+                *errmsg = myerrmsg;
+            return NULL;
+        }
+
+        collection = getCollection( profileName );
+        verify( collection );
+        return collection;
     }
 
 } // namespace mongo
--- src/mongo/db/introspect.h
+++ src/mongo/db/introspect.h
@@ -3,7 +3,6 @@
 
 /**
 *    Copyright (C) 2008 10gen Inc.
-*    Copyright (C) 2013 Tokutek Inc.
 *
 *    This program is free software: you can redistribute it and/or  modify
 *    it under the terms of the GNU Affero General Public License, version 3,
@@ -16,17 +15,31 @@
 *
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    As a special exception, the copyright holders give permission to link the
+*    code of portions of this program with the OpenSSL library under certain
+*    conditions as described in each individual source file and distribute
+*    linked combinations including the program with the OpenSSL library. You
+*    must comply with the GNU Affero General Public License in all respects for
+*    all of the code used other than as permitted herein. If you modify file(s)
+*    with this exception, you may extend this exception to your version of the
+*    file(s), but you are not obligated to do so. If you do not wish to do so,
+*    delete this exception statement from your version. If you delete this
+*    exception statement from all source files in the program, then also delete
+*    it in the license file.
 */
 
 #pragma once
 
-#include "mongo/db/client.h"
-#include "mongo/db/collection.h"
+#include <string>
 
-#include "mongo/pch.h"
-#include "jsobj.h"
+#include "mongo/db/curop.h"
+#include "mongo/db/jsobj.h"
 
 namespace mongo {
+
+    class Collection;
+    class Database;
 
     /* --- profiling --------------------------------------------
        do when database->profile is set
@@ -41,6 +54,8 @@ namespace mongo {
      * @param   force   Always create the collection if it does not exist
      * @return  Collection for the newly created collection, or NULL on error
     **/
-    Collection *getOrCreateProfileCollection(Database *db, bool force = false);
+    Collection* getOrCreateProfileCollection(Database *db,
+                                             bool force = false,
+                                             std::string* errmsg = NULL);
 
 } // namespace mongo
