06ba94e02b1c0a139a8569e48a2af0592265dfb3
christianrober <christianrober@gmail.com>
2014-10-03 13:54:26 -0400

    Adding audit command test that uses count to check for bogus auth.

---
+++ jstests/audit/audit_authz_command.js
@@ -0,0 +1,39 @@
+// test that authzInProg gets called.
+
+if (TestData.testData !== undefined) {
+    load(TestData.testData + '/audit/_audit_helpers.js');
+} else {
+    load('jstests/audit/_audit_helpers.js');
+}
+
+var testDBName = 'audit_authz_in_prog';
+
+auditTest(
+    'authzInProg',
+    function(m) {
+        createAdminUserForAudit(m);
+        var testDB = m.getDB(testDBName);
+        var user = createNoPermissionUserForAudit(m, testDB);
+
+        // User (tom) with no permissions logs in.
+        var r = testDB.auth('tom', 'tom');
+        assert(r);
+
+        // Tom tries to perfom a command.
+        testDB.runCommand( {count : 'foo'} );
+
+        // Tom logs out.
+        testDB.logout();
+
+        // Verify that audit event was inserted.
+        auditColl = getAuditEventsCollection(m, undefined, true);
+        assert.eq(1, auditColl.count({
+            atype: "authCheck",
+            ts: withinTheLastFewSeconds(),
+            'params.ns': testDBName + '.' + 'foo',
+            'params.command': 'count',
+            result: 13, // <-- Unauthorized error, see error_codes.err...
+        }), "FAILED, audit log: " + tojson(auditColl.find().toArray()));
+    },
+    { auth:"" }
+);
